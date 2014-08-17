4d95c38a3ec7aaddc16264c470e6a15c86db226e
Neravin <neravind@gmail.com>
2014-08-17 19:56:10 +0400

    add advertisements to client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,8 @@ class ClientsController < ApplicationController
   skip_before_action :authorize#, only: :composition
   before_action :signed_in_client, only: [:show, :edit, :update]
   before_action :correct_client,   only: [:show, :edit, :update]
-
+  before_action :set_client, only: [:show, :edit, :update, :destroy]
+  
   # GET /clients/new
   def new
     @client = Client.new
