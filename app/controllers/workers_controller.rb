d0174670af5e4ee1b7c38125b1f99d245a07f496
Neravin <neravind@gmail.com>
2014-08-21 13:17:44 +0400

    edit and update for workers

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,5 +1,7 @@
 class WorkersController < ApplicationController
 	before_action :set_worker, only: [:show, :edit, :update, :destroy]
+	before_action :correct_worker, only: [:edit, :update]
+
 	def index
 		@workers = Worker.all
 	end
@@ -23,13 +25,36 @@ class WorkersController < ApplicationController
   	end
 	end
 
-	private
-		def set_worker
-			@worker = Worker.find(params[:id])
-		end
-		
-		def worker_params
-			params.require(:worker).permit(:name, :description, :price, :city, :address)
-		end
+	def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @worker.update(worker_params)
+        format.html { redirect_to current_client, notice: 'Объявление успешно обновлено' }
+        #format.json { render :show, status: :ok, location: @worker }
+      else
+        format.html { render :edit }
+        #format.json { render json: @worker.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+	def set_worker
+		@worker = Worker.find(params[:id])
+	end
+	
+	def worker_params
+		params.require(:worker).permit(:name, :description, :price, :city, :address)
+	end
+
+	def correct_worker
+    if current_client
+      redirect_to('') unless current_client.workers.find_by_id(params[:id])
+    else
+      redirect_to('')
+    end
+  end
 
 end
