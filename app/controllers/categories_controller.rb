9253a59b67ba0bf2444e919c9e78512334fa9616
flash <isinok5@gmail.com>
2014-08-24 15:56:23 +0400

    Set_category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 class CategoriesController < ApplicationController
-
+	 before_action :set_category, only: [:show, :edit, :update, :destroy]
+	def new
+		@category = Category.new
+	end
+	
 	def edit	
 	end
+	
 	def create
 		@category = Category.new(category_params)
 		respond_to do |format|
@@ -14,10 +19,21 @@ class CategoriesController < ApplicationController
 		end
 	end
 
+	def update
+		respond_to do |format|
+  		if @category.update(category_params)
+  			format.html { redirect_to admin_routes_index_path }
+  		else
+  			format.html { render :edit }
+  		end
+  	end
+	end
+
 	private
+		 def set_category
+    		@category = Category.find(params[:id])
+  		end
 		def category_params
     		params.require(:category).permit(:name)
   		end
-
-
 end
