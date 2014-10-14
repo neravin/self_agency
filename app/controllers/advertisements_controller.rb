c9229c47193790683fc28811f82f835076c925bf
flash <isinok5@gmail.com>
2014-10-14 18:48:34 +0400

    After error search

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -18,7 +18,8 @@ class AdvertisementsController < ApplicationController
     if buf
       @advertisements = buf.page(params[:page]).per_page(3)
     end
-	end
+    
+    
 
   def new
     @advertisement = Advertisement.new
